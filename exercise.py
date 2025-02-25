044e87ed730c7547e15a545163482762cf2a5ee7
Ayokayzy <theayokayzy1@gmail.com>
2025-02-25 23:55:08 +0100

    Writing string to Redis

--- exercise.py
+++ exercise.py
@@ -5,7 +5,7 @@ exercise.py
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -14,10 +14,58 @@ class Cache:
     """
 
     def __init__(self):
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         rand_key = str(uuid.uuid4())
-        self.__redis.set(rand_key, data)
+        self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
+        """
+        Get data from the cache.
+
+        Args:
+            key (str): The key associated with the data.
+            fn (Optional[Callable]): A callable function to transform the
+            retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float]: The retrieved data.
+        """
+        value = self._redis.get(key)
+        if fn and value is not None:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Get a string from the cache.
+
+        Args:
+            key (str): The key associated with the string.
+
+        Returns:
+            str: The retrieved string.
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8') if value else ""
+
+    def get_int(self, key: str) -> int:
+        """
+        Get an integer from the cache.
+
+        Args:
+            key (str): The key associated with the integer.
+
+        Returns:
+            int: The retrieved integer.
+        """
+        value = self._redis.get(key)
+        try:
+            return int(value.decode('utf-8')) if value else 0
+        except ValueError:
+            return 0
